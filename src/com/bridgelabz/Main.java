e8ef18d7598948b219cf6fc53a053dab7d4ce0cf
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-25 01:58:05 +0530

    REFACTORED

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,6 +6,32 @@ public class Main extends GenericClass {
 
 
     public static void main(String[] args) {
+        /**
+         * To compute max among three Intgers
+         * @param x Integer
+         * @param y Integer
+         * @param z Integer
+         * @return max
+         */
+        System.out.println("Greatest: "+getMax(10,12,43));
+
+        /**
+         * To compute max among three double number
+         * @param x Double
+         * @param y Double
+         * @param z Double
+         * @return max
+         */
+        System.out.println("Greatest: "+getMax(87.21,32.98,63.45));
+
+        /**
+         * To compute max among three Strings i.e the String with the furthest characters
+         * @param x Integer
+         * @param y Integer
+         * @param z Integer
+         * @return max
+         */
+
         String word1="Apple";
         String word2="Banana";
         String word3="Peach";
