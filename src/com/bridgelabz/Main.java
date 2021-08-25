496081bd68e6c81714425cd911e1bec837cea76e
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-25 08:24:29 +0530

    ADDED: UC-5

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -13,15 +13,16 @@ public class Main extends GenericClass {
 
         Integer[] intArray = {5, 3, 7, 8, 9};
         new GenericClass(intArray).toPrint();
+        getMax(intArray);
 
 
         Double[] doubleArray = {42.5, 35.6, 72.5, 89.5, 94.3};
         new GenericClass(doubleArray).toPrint();
+        getMax(doubleArray);
 
-
-        String[] stringArray = {"Apple", "Banana", "Peach", "Watermelon"};
+        String[] stringArray = {"Apple", "Banana", "Peach", "Watermelon","Zolo"};
         new GenericClass(stringArray).toPrint();
-
+        getMax(stringArray);
 
     }
 }
